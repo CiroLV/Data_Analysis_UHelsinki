7107391d8d8b0a32c6882393621679c271cc9c20
Ciro LV <ciro.lopezv@gmail.com>
2020-08-20 13:17:39 +0100

    code test

--- hy-data-analysis-with-python-2020/part01-e11_interleave/src/interleave.py
+++ hy-data-analysis-with-python-2020/part01-e11_interleave/src/interleave.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def interleave(*lists):
-    return []
+    final_list = []
+    for t in zip(*lists):
+        new_element = list(t)
+        final_list.extend(new_element)
+    return final_list
 
 def main():
     print(interleave([1, 2, 3], [20, 30, 40], ['a', 'b', 'c']))
