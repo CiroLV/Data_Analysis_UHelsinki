4030cf166e50d29a2ae875bf2685e99f9da715f3
Ciro LV <ciro.lopezv@gmail.com>
2020-08-26 15:54:22 +0100

    new script

--- hy-data-analysis-with-python-2020/part01-e13_reverse_dictionary/src/reverse_dictionary.py
+++ hy-data-analysis-with-python-2020/part01-e13_reverse_dictionary/src/reverse_dictionary.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
 def reverse_dictionary(d):
-    return {}
+    dict_FtE= {}
+    for key in d.keys():
+        for x in d[key]:
+            if x in dict_FtE.keys():
+                dict_FtE[x].append(key)
+            else:
+                dict_FtE[x] = [key]
+    return dict_FtE
 
 def main():
+    d={'move': ['liikuttaa'], 'hide': ['piilottaa','salata'], 'six': ['kuusi'], 'fir': ['kuusi']}
+    reverse_dictionary(d)
     pass
 
 if __name__ == "__main__":
